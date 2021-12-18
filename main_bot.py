efee75cc45e3979392b83e2bbdd0b933db37a813
Egor <egorjkee96@gmail.com>
2021-12-18 21:06:15 +0300

    webhook

--- main_bot.py
+++ main_bot.py
@@ -1,6 +1,37 @@
+# import telebot
+# from handlers import bot
+# from settings import TELEGRAM_TOKEN
+
+# if __name__ == '__main__':
+#     bot.polling(none_stop=True)
+
 import telebot
-from handlers import bot
+import cherrypy
 from settings import TELEGRAM_TOKEN
 
-if __name__ == '__main__':
-    bot.polling(none_stop=True)
+WEBHOOK_HOST = '178.155.4.121'
+WEBHOOK_PORT = 443  # 443, 80, 88 или 8443 (порт должен быть открыт!)
+WEBHOOK_LISTEN = '178.155.4.121'  # На некоторых серверах придется указывать такой же IP, что и выше
+
+WEBHOOK_SSL_CERT = './webhook_cert.pem'  # Путь к сертификату
+WEBHOOK_SSL_PRIV = './webhook_pkey.pem'  # Путь к приватному ключу
+
+WEBHOOK_URL_BASE = "https://%s:%s" % (WEBHOOK_HOST, WEBHOOK_PORT)
+WEBHOOK_URL_PATH = "/%s/" % (TELEGRAM_TOKEN)
+
+bot = telebot.TeleBot(TELEGRAM_TOKEN)
+
+class WebhookServer(object):
+    @cherrypy.expose
+    def index(self):
+        if 'content-length' in cherrypy.request.headers and \
+                        'content-type' in cherrypy.request.headers and \
+                        cherrypy.request.headers['content-type'] == 'application/json':
+            length = int(cherrypy.request.headers['content-length'])
+            json_string = cherrypy.request.body.read(length).decode("utf-8")
+            update = telebot.types.Update.de_json(json_string)
+            # Эта функция обеспечивает проверку входящего сообщения
+            bot.process_new_updates([update])
+            return ''
+        else:
+            raise cherrypy.HTTPError(403)
