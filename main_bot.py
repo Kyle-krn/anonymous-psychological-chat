2795e9e6820c391bc72518b46fe0fa86f2d81a04
Egor <egorjkee96@gmail.com>
2021-12-19 18:40:21 +0300

    change import bot

--- main_bot.py
+++ main_bot.py
@@ -11,6 +11,7 @@ import flask
 import logging
 import time
 from settings import TELEGRAM_TOKEN, HOST
+from handlers import bot
 
 
 
@@ -36,8 +37,6 @@ if __name__ == '__main__':
     logger = telebot.logger
     telebot.logger.setLevel(logging.INFO)
 
-    bot = telebot.TeleBot(TELEGRAM_TOKEN)
-
     app = flask.Flask(__name__)
 
 
@@ -60,17 +59,6 @@ if __name__ == '__main__':
 
 
     # Handle '/start' and '/help'
-    @bot.message_handler(commands=['help', 'start'])
-    def send_welcome(message):
-        bot.reply_to(message,
-                    ("Hi there, I am EchoBot.\n"
-                    "I am here to echo your kind words back to you."))
-
-
-    # Handle all other messages
-    @bot.message_handler(func=lambda message: True, content_types=['text'])
-    def echo_message(message):
-        bot.reply_to(message, message.text)
 
 
     # Remove webhook, it fails sometimes the set if there is a previous webhook
